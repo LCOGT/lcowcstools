e00d9400ab19dc1e946831fa019f2cd265cf80ea
Daniel Harbeck <drharbeck@gmail.com>
2019-01-23 19:40:06 -0800

    Refactoring Cagtalog provider so I can have an abstract itnerface instead of hardcoding the catalog method. should help with the general usability.

--- src/CatalogMatcher.py
+++ src/CatalogMatcher.py
@@ -4,13 +4,11 @@ from astropy.table import Table
 from astropy.wcs import WCS
 from astropy.coordinates import SkyCoord
 from astropy import units as u
-import os
-import sqlite3
 import numpy as np
 import math
 import matplotlib.pyplot as plt
 
-
+from src.ReferenceCatalogProvider import refcat2
 
 log = logging.getLogger(__name__)
 logging.basicConfig(level=getattr(logging, 'DEBUG'),
@@ -30,7 +28,7 @@ class CatalogMatcher:
     '''
 
     @staticmethod
-    def createMatchedCatalogForLCOe91 (imagepath, refcat2db, matchradius=5):
+    def createMatchedCatalogForLCOe91 (imagepath, referenceCatalogProvider, matchradius=5):
         ''' Automatically load source catalog from an LCO e91 processed file, fetch a reference catalog, and return
          a matchedcatalog object.'''
 
@@ -52,7 +50,7 @@ class CatalogMatcher:
         e91image.close()
 
         # fetch a reference catalog:
-        referenceCatalogProvider = refcat2(refcat2db)
+        #referenceCatalogProvider = refcat2(refcat2db)
         referenceCatalog = referenceCatalogProvider.get_reference_catalog(ra,dec,0.25)
 
         matchedCatalog = CatalogMatcher()
@@ -153,88 +151,6 @@ class CatalogMatcher:
 
 
 
-class refcat2:
-    # Interface to query consolidat3ed sqlite3 db file that was geernated from Tonry (2018) refcat2
-
-
-    FILTERMAPPING = {}
-    FILTERMAPPING['gp'] = {'refMag': 'g', 'colorTerm': 0.0, 'airmassTerm': 0.20, 'defaultZP': 0.0}
-    FILTERMAPPING['rp'] = {'refMag': 'r', 'colorTerm': 0.0, 'airmassTerm': 0.12, 'defaultZP': 0.0}
-    FILTERMAPPING['ip'] = {'refMag': 'i', 'colorTerm': 0.0, 'airmassTerm': 0.08, 'defaultZP': 0.0}
-    FILTERMAPPING['zp'] = {'refMag': 'z', 'colorTerm': 0.0, 'airmassTerm': 0.05, 'defaultZP': 0.0}
-
-    ###  PS to SDSS color transformations according to  Finkbeiner 2016
-    ###  http://iopscience.iop.org/article/10.3847/0004-637X/822/2/66/meta#apj522061s2-4 Table 2
-    ###  Note that this transformation is valid for stars only. For the purpose of photometric
-    ###  calibration, it is desirable to select point sources only from the input catalog.
-
-    ## Why reverse the order of the color term entries? Data are entered in the order as they are
-    ## shown in paper. Reverse after the fact to avoid confusion when looking at paper
-
-    ps1colorterms = {}
-    ps1colorterms['g'] = [-0.01808, -0.13595, +0.01941, -0.00183][::-1]
-    ps1colorterms['r'] = [-0.01836, -0.03577, +0.02612, -0.00558][::-1]
-    ps1colorterms['i'] = [+0.01170, -0.00400, +0.00066, -0.00058][::-1]
-    ps1colorterms['z'] = [-0.01062, +0.07529, -0.03592, +0.00890][::-1]
-
-
-    def __init__(self, dbfile):
-        if (dbfile is None) or (not os.path.isfile(dbfile)):
-            log.error("Unable to find reference catalog: %s" % (str(dbfile)))
-            self.dbfile = None
-            return
-        self.dbfile = dbfile
-
-    def isInCatalogFootprint(self, ra, dec):
-        return True
-
-
-    def PStoSDSS(self, table):
-        """
-        Modify table in situ from PS1 to SDSS, requires column names compatible with ps1colorterms definition.
-
-        :param table:
-        :return: modified table.
-        """
-        if table is not None:
-            pscolor = table['g'] - table['i']
-            for filter in self.ps1colorterms:
-                colorcorrection = np.polyval(self.ps1colorterms[filter], pscolor)
-                table[filter] -= colorcorrection
-
-        return table
-
-
-    def get_reference_catalog(self, ra, dec, radius, overwrite_select=False):
-
-        rows = None
-        try:
-            connection = sqlite3.connect(self.dbfile)
-            cursor = connection.cursor()
-            if (not radius == None and radius > 0):
-                min_dec = dec - radius
-                max_dec = dec + radius
-                min_ra = ra - radius / math.cos(math.radians(dec))
-                max_ra = ra + radius / math.cos(math.radians(dec))
-
-
-            sql_command = 'select sources.RA, sources.Dec, sources.g,sources.r,sources.i, sources.z from sources, positions ' \
-                          'where positions.ramin >= {ramin} and positions.ramax <= {ramax} ' \
-                          'and positions.decmin >= {decmin} and positions.decmax <= {decmax} ' \
-                          'and positions.objid = sources.objid'
-
-            sql_command = sql_command.format(ramin=min_ra, ramax=max_ra, decmin=min_dec, decmax=max_dec)
-            cursor.execute(sql_command)
-            rows = np.asarray(cursor.fetchall())
-            table = Table (rows, names=['RA','Dec','g','r','i','z'])
-            cursor.close()
-        except:
-            log.exception("While trying to read from database:")
-            return None
-
-        table = self.PStoSDSS(table)
-        log.debug("Reference Catalog has  %d entries" % len(table))
-        return table
 
 
 
@@ -252,7 +168,7 @@ class SIPOptimizer:
         return matchedCatalog.gerrms()
 
     def improveSIP (self):
-        initialGuess = np.zeros[maxorder]
+        initialGuess = np.zeros[self.maxorder]
         scipy.optimize.minimize (self.merritFunction, initialGuess)
 
 
@@ -260,7 +176,7 @@ class SIPOptimizer:
 
 if __name__ == '__main__':
     matchedCatalog = CatalogMatcher.createMatchedCatalogForLCOe91('/archive/engineering/lsc/fa15/20190122/processed/lsc1m005-fa15-20190122-0323-e91.fits.fz',
-                                                 '/nfs/AstroCatalogs/Atlas-refcat2/refcat2.db', 3)
+                                                 refcat2('/nfs/AstroCatalogs/Atlas-refcat2/refcat2.db'), 3)
 
     log.info ("Residual error of matched catalog: % 7.3f" % matchedCatalog.getUpdatedRMS())
     matchedCatalog.diagnosticPlots('test')
---
+++ src/ReferenceCatalogProvider.py
@@ -0,0 +1,104 @@
+import abc
+import numpy as np
+import sqlite3
+import math
+from astropy.table import Table
+import logging
+import os.path
+
+
+log = logging.getLogger(__name__)
+logging.basicConfig(level=getattr(logging, 'DEBUG'),
+                    format='%(asctime)s.%(msecs).03d %(levelname)7s: %(module)20s: %(message)s')
+
+
+class ReferenceCatalogProvider (metaclass=abc.ABCMeta):
+
+    @abc.abstractmethod
+    def get_reference_catalog(self, ra, dec, radius):
+        pass
+
+
+
+class refcat2 (ReferenceCatalogProvider):
+    # Interface to query consolidat3ed sqlite3 db file that was geernated from Tonry (2018) refcat2
+
+
+    FILTERMAPPING = {}
+    FILTERMAPPING['gp'] = {'refMag': 'g', 'colorTerm': 0.0, 'airmassTerm': 0.20, 'defaultZP': 0.0}
+    FILTERMAPPING['rp'] = {'refMag': 'r', 'colorTerm': 0.0, 'airmassTerm': 0.12, 'defaultZP': 0.0}
+    FILTERMAPPING['ip'] = {'refMag': 'i', 'colorTerm': 0.0, 'airmassTerm': 0.08, 'defaultZP': 0.0}
+    FILTERMAPPING['zp'] = {'refMag': 'z', 'colorTerm': 0.0, 'airmassTerm': 0.05, 'defaultZP': 0.0}
+
+    ###  PS to SDSS color transformations according to  Finkbeiner 2016
+    ###  http://iopscience.iop.org/article/10.3847/0004-637X/822/2/66/meta#apj522061s2-4 Table 2
+    ###  Note that this transformation is valid for stars only. For the purpose of photometric
+    ###  calibration, it is desirable to select point sources only from the input catalog.
+
+    ## Why reverse the order of the color term entries? Data are entered in the order as they are
+    ## shown in paper. Reverse after the fact to avoid confusion when looking at paper
+
+    ps1colorterms = {}
+    ps1colorterms['g'] = [-0.01808, -0.13595, +0.01941, -0.00183][::-1]
+    ps1colorterms['r'] = [-0.01836, -0.03577, +0.02612, -0.00558][::-1]
+    ps1colorterms['i'] = [+0.01170, -0.00400, +0.00066, -0.00058][::-1]
+    ps1colorterms['z'] = [-0.01062, +0.07529, -0.03592, +0.00890][::-1]
+
+
+    def __init__(self, dbfile):
+        if (dbfile is None) or (not os.path.isfile(dbfile)):
+            log.error("Unable to find reference catalog: %s" % (str(dbfile)))
+            self.dbfile = None
+            return
+        self.dbfile = dbfile
+
+    def isInCatalogFootprint(self, ra, dec):
+        return True
+
+
+    def PStoSDSS(self, table):
+        """
+        Modify table in situ from PS1 to SDSS, requires column names compatible with ps1colorterms definition.
+
+        :param table:
+        :return: modified table.
+        """
+        if table is not None:
+            pscolor = table['g'] - table['i']
+            for filter in self.ps1colorterms:
+                colorcorrection = np.polyval(self.ps1colorterms[filter], pscolor)
+                table[filter] -= colorcorrection
+
+        return table
+
+
+    def get_reference_catalog(self, ra, dec, radius, overwrite_select=False):
+
+        rows = None
+        try:
+            connection = sqlite3.connect(self.dbfile)
+            cursor = connection.cursor()
+            if (not radius == None and radius > 0):
+                min_dec = dec - radius
+                max_dec = dec + radius
+                min_ra = ra - radius / math.cos(math.radians(dec))
+                max_ra = ra + radius / math.cos(math.radians(dec))
+
+
+            sql_command = 'select sources.RA, sources.Dec, sources.g,sources.r,sources.i, sources.z from sources, positions ' \
+                          'where positions.ramin >= {ramin} and positions.ramax <= {ramax} ' \
+                          'and positions.decmin >= {decmin} and positions.decmax <= {decmax} ' \
+                          'and positions.objid = sources.objid'
+
+            sql_command = sql_command.format(ramin=min_ra, ramax=max_ra, decmin=min_dec, decmax=max_dec)
+            cursor.execute(sql_command)
+            rows = np.asarray(cursor.fetchall())
+            table = Table (rows, names=['RA','Dec','g','r','i','z'])
+            cursor.close()
+        except:
+            log.exception("While trying to read from database:")
+            return None
+
+        table = self.PStoSDSS(table)
+        log.debug("Reference Catalog has  %d entries" % len(table))
+        return table
