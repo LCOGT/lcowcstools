a29fda6683cb70e14a7740f13cf006e365af4058
Daniel Harbeck <drharbeck@gmail.com>
2019-01-25 16:41:35 -0800

    sep extarction for non e91 products

--- src/CatalogMatcher.py
+++ src/CatalogMatcher.py
@@ -13,7 +13,7 @@ from random import random
 
 from src.ReferenceCatalogProvider import refcat2, gaiaonline
 
-#from src.ReferenceCatalogProvider import gaiaonline
+# from src.ReferenceCatalogProvider import gaiaonline
 from src.SourceCatalogProvider import e91SourceCatalogProvider, blindGaiaAstrometrySourceCatalogProvider
 
 log = logging.getLogger(__name__)
@@ -53,9 +53,6 @@ class CatalogMatcher:
         matchedCatalog.matchCatalogs(sourceCatalog, referenceCatalog, image_wcs, matchradius)
         return matchedCatalog
 
-
-
-
     def matchCatalogs(self, source=None, reference=None, wcs=None, matchradius=5):
         ''' match input catalogs.
         If no new catalogs are given, the match will be done on the chached catalogs of the class.
@@ -85,12 +82,12 @@ class CatalogMatcher:
 
             matchcondition = (distance < matchradius)
             self.matchedCatalog = Table([self.source['x'][idx][matchcondition],
-                                self.source['y'][idx][matchcondition],
-                                self.reference['RA'][matchcondition],
-                                self.reference['Dec'][matchcondition],
-                                distance[matchcondition]],
-                               names=['x', 'y', 'RA', 'Dec', 'distarcsec']
-                               )
+                                         self.source['y'][idx][matchcondition],
+                                         self.reference['RA'][matchcondition],
+                                         self.reference['Dec'][matchcondition],
+                                         distance[matchcondition]],
+                                        names=['x', 'y', 'RA', 'Dec', 'distarcsec']
+                                        )
 
             log.debug("Catalog matched %d entries" % len(self.matchedCatalog))
 
@@ -105,10 +102,10 @@ class CatalogMatcher:
 
         if usewcs is not None:
             self.wcs = usewcs
-            #log.debug ("WCS updated for MatchedCatalog")
+            # log.debug ("WCS updated for MatchedCatalog")
         else:
             pass
-            #log.info ("WCS not updated")
+            # log.info ("WCS not updated")
 
         sourcera, sourcedec = self.wcs.all_pix2world(self.matchedCatalog['x'], self.matchedCatalog['y'], 1)
 
@@ -118,8 +115,8 @@ class CatalogMatcher:
 
         self.matchedCatalog['distarcsec'] = referenceSkyCoords.separation(sourceSkyCoords).arcsecond
 
-        result = math.sqrt(np.sum(self.matchedCatalog['distarcsec']**2)   / len( self.matchedCatalog['distarcsec']))
-        #log.info ("WCS CRVAL % 12.9f % 12.9f , Source RA / Dec [0] %f %f  Merrit %f" % (self.wcs.wcs.crval[0], self.wcs.wcs.crval[1], sourcera[0], sourcedec[0],  result))
+        result = math.sqrt(np.sum(self.matchedCatalog['distarcsec'] ** 2) / len(self.matchedCatalog['distarcsec']))
+        # log.info ("WCS CRVAL % 12.9f % 12.9f , Source RA / Dec [0] %f %f  Merrit %f" % (self.wcs.wcs.crval[0], self.wcs.wcs.crval[1], sourcera[0], sourcedec[0],  result))
 
         return result
 
@@ -129,44 +126,44 @@ class CatalogMatcher:
 
         sourcera, sourcedec = self.wcs.all_pix2world(self.matchedCatalog['x'], self.matchedCatalog['y'], 1)
 
-        deccor = math.cos (self.wcs.wcs.crval[1]*math.pi/180)
+        deccor = math.cos(self.wcs.wcs.crval[1] * math.pi / 180)
 
-        #plt.subplot(projection=self.wcs)
+        plt.subplot(projection=self.wcs)
         plt.plot(sourcera, sourcedec, '.')
         plt.plot(self.matchedCatalog['RA'], self.matchedCatalog['Dec'], '.')
-        plt.xlabel ("RA")
-        plt.ylabel ("DEC")
+        plt.xlabel("RA")
+        plt.ylabel("DEC")
         plt.savefig("%s_RADEC.png" % basename)
         plt.close()
 
         plt.clf()
-        plt.subplot (4,1,1)
-        plt.plot(self.matchedCatalog['x'] - self.wcs.wcs.crpix[0], (self.matchedCatalog['RA'] - sourcera)*3600. / deccor, '.')
+        plt.subplot(4, 1, 1)
+        plt.plot(self.matchedCatalog['x'] - self.wcs.wcs.crpix[0],
+                 (self.matchedCatalog['RA'] - sourcera) * 3600. / deccor, '.')
         plt.xlabel("X [pixels]")
         plt.ylabel("residual RA [\'\']")
-        plt.ylim([-0.75,0.75])
+        plt.ylim([-1.75, 1.75])
 
-
-        plt.subplot (4,1,2)
-        plt.plot(self.matchedCatalog['x'] - self.wcs.wcs.crpix[0], (self.matchedCatalog['Dec'] - sourcedec)*3600., '.')
+        plt.subplot(4, 1, 2)
+        plt.plot(self.matchedCatalog['x'] - self.wcs.wcs.crpix[0], (self.matchedCatalog['Dec'] - sourcedec) * 3600.,
+                 '.')
         plt.xlabel("X [pixels]")
         plt.ylabel("resiudal Dec [\'\']")
-        plt.ylim([-0.75,0.75])
+        plt.ylim([-1.75, 1.75])
 
-
-        plt.subplot (4,1,3)
-        plt.plot(self.matchedCatalog['y'] - self.wcs.wcs.crpix[1], (self.matchedCatalog['RA'] - sourcera)*3600. / deccor, '.')
+        plt.subplot(4, 1, 3)
+        plt.plot(self.matchedCatalog['y'] - self.wcs.wcs.crpix[1],
+                 (self.matchedCatalog['RA'] - sourcera) * 3600. / deccor, '.')
         plt.xlabel("Y [pixels]")
         plt.ylabel("residual ra [\'\']")
-        plt.ylim([-0.75,0.75])
+        plt.ylim([-1.75, 1.75])
 
-
-        plt.subplot (4,1,4)
-        plt.plot(self.matchedCatalog['y'] - self.wcs.wcs.crpix[1], (self.matchedCatalog['Dec'] - sourcedec)*3600., '.')
+        plt.subplot(4, 1, 4)
+        plt.plot(self.matchedCatalog['y'] - self.wcs.wcs.crpix[1], (self.matchedCatalog['Dec'] - sourcedec) * 3600.,
+                 '.')
         plt.xlabel("Y [pixels]")
         plt.ylabel("residual dec [\'\']")
-        plt.ylim([-0.75,0.75])
-
+        plt.ylim([-1.75, 1.75])
 
         plt.savefig("%s_residuals.png" % basename, dpi=200)
         plt.close()
@@ -189,18 +186,20 @@ class SIPOptimizer:
 
         # bootstrap the initial SIP wcs
         self.maxorder = maxorder
-        log.info (self.matchedCatalog.wcs.wcs.ctype)
+        log.info(self.matchedCatalog.wcs.wcs.ctype)
         crval = self.matchedCatalog.wcs.wcs.crval
         cd = self.matchedCatalog.wcs.wcs.cd
 
+        self.wcsfitparams = [crval[0], crval[1], cd[0][0], cd[0][1], cd[1][0], cd[1][1]]
 
-        self.wcsfitparams = [crval[0] , crval[1], cd[0][0], cd[0][1], cd[1][0], cd[1][1], 0,0,0,0,0,0]
-        if maxorder >2:
-            self.wcsfitparams.extend ([0,0,0,0])
+        if maxorder > 1:
+            self.wcsfitparams.extend([0, 0, 0, 0, 0, 0])
+
+        if maxorder > 2:
+            self.wcsfitparams.extend([0, 0, 0, 0])
 
         merrit = SIPOptimizer.merritFunction(self.wcsfitparams, self.matchedCatalog)
         log.info("SIPOptimizer init: merrit function is %12.7f" % (merrit))
-
 
     @staticmethod
     def merritFunction(sipcoefficients, matchedCatalog):
@@ -212,43 +211,53 @@ class SIPOptimizer:
         matchedCatalog.wcs.wcs.cd[1][0] = sipcoefficients[4]
         matchedCatalog.wcs.wcs.cd[1][1] = sipcoefficients[5]
 
+
         m = 3
         sip_a = np.zeros((m + 1, m + 1), np.double)
         sip_b = np.zeros((m + 1, m + 1), np.double)
 
-        sip_a[1][1]=sipcoefficients[6]
-        sip_a[2][0]=sipcoefficients[7]
-        sip_a[0][2]=sipcoefficients[8]
+        if len(sipcoefficients) > 6:
+            sip_a[1][1] = sipcoefficients[6]
+            sip_a[2][0] = sipcoefficients[7]
+            sip_a[0][2] = sipcoefficients[8]
+            sip_b[1][1] = sipcoefficients[9]
+            sip_b[2][0] = sipcoefficients[10]
+            sip_b[0][2] = sipcoefficients[11]
 
-        sip_b[1][1]=sipcoefficients[9]
-        sip_b[2][0]=sipcoefficients[10]
-        sip_b[0][2]=sipcoefficients[11]
+        if len(sipcoefficients) > 12:
+            sip_a[3][0] = sipcoefficients[12]
+            sip_a[0][3] = sipcoefficients[13]
+            sip_b[3][0] = sipcoefficients[14]
+            sip_b[0][3] = sipcoefficients[15]
 
-        if len (sipcoefficients) > 12:
-            sip_a[3][0] =  sipcoefficients[12]
-            sip_a[0][3] =  sipcoefficients[13]
-            sip_b[3][0] =  sipcoefficients[14]
-            sip_b[0][3] =  sipcoefficients[15]
-
-
-        sip = Sip (sip_a,sip_b,
-                   None, None,  matchedCatalog.wcs.wcs.crval)
+        sip = Sip(sip_a, sip_b,
+                  None, None, matchedCatalog.wcs.wcs.crval)
         matchedCatalog.wcs.sip = sip
 
         matchedCatalog.wcs.wcs.ctype = ["RA---TAN", "DEC--TAN"]
         merrit = matchedCatalog.updateWCSandUpdateRMS(matchedCatalog.wcs)
-        #log.debug("% 12.9f % 12.9f f % 12.7f" % (sipcoefficients[0], sipcoefficients[1], merrit))
+        # log.debug("% 12.9f % 12.9f f % 12.7f" % (sipcoefficients[0], sipcoefficients[1], merrit))
         return merrit
 
     def improveSIP(self):
 
+        deltas = [
+            0.001, 0.001,  # CRVAL
+            1e-5, 1e-5, 1e-5, 1e-5,  # CD Matrix
+            1e-8, 1e-8, 1e-8,  # SIP order 2 A
+            1e-8, 1e-8, 1e-8,  # SIP order 2
+            1e-10, 1e-10, 1e-10, 1e-10,  # SIP ORDER 3
+        ]
+
+        deltas = deltas[:len(self.wcsfitparams)]
         bestfit = optimize.minimize(SIPOptimizer.merritFunction, self.wcsfitparams, args=(self.matchedCatalog))
+        SIPOptimizer.merritFunction(bestfit.x, self.matchedCatalog)
         log.info("Optimizer return        %s" % bestfit)
 
 
 if __name__ == '__main__':
     refcat = refcat2('/nfs/AstroCatalogs/Atlas-refcat2/refcat2.db')
-    #refcat = gaiaonline()
+    # refcat = gaiaonline()
 
     # Sinsitro
     # matchedCatalog = CatalogMatcher.createMatchedCatalogForLCOe91(
@@ -257,8 +266,8 @@ if __name__ == '__main__':
 
     # TLV AGU
     matchedCatalog = CatalogMatcher.createMatchedCatalogForLCOe91(
-        '/archive/engineering/tlv/ak10/20190124/raw/tlv1m0XX-ak10-20190124-0012-e00.fits.fz',
-        refcat, 1)
+        '/archive/engineering/lsc/ak01/20190123/raw/lsc1m009-ak01-20190123-0257-e00.fits.fz',
+        refcat, 10)
 
     # matchedCatalog = CatalogMatcher.createMatchedCatalogForLCOe91(
     #     '/archive/engineering/lsc/kb95/20190114/processed/lsc0m409-kb95-20190114-0100-e91.fits.fz',
@@ -268,12 +277,17 @@ if __name__ == '__main__':
     matchedCatalog.diagnosticPlots('test_prefit')
     opt.improveSIP()
 
-
-    matchedCatalog.matchCatalogs(matchradius=0.5)
+    matchedCatalog.matchCatalogs(matchradius=10)
     opt = SIPOptimizer(matchedCatalog, maxorder=2)
     opt.improveSIP()
+
+    matchedCatalog.matchCatalogs(matchradius=2)
+    opt = SIPOptimizer(matchedCatalog, maxorder=2)
+    opt.improveSIP()
+
+    matchedCatalog.matchCatalogs(matchradius=1)
+    opt = SIPOptimizer(matchedCatalog, maxorder=2)
+    opt.improveSIP()
+
     matchedCatalog.diagnosticPlots('test_postiteration1')
-    log.info (matchedCatalog.wcs)
-
-
-
+    log.info(matchedCatalog.wcs)
--- src/SourceCatalogProvider.py
+++ src/SourceCatalogProvider.py
@@ -5,13 +5,16 @@ import numpy as np
 from astropy.io import fits
 from astropy.wcs import WCS
 from astropy.table import Table
+import sep
 
 import logging
+
 log = logging.getLogger(__name__)
 logging.basicConfig(level=getattr(logging, 'DEBUG'),
                     format='%(asctime)s.%(msecs).03d %(levelname)7s: %(module)20s: %(message)s')
 
-class SourceCatalogProvider (metaclass=abc.ABCMeta):
+
+class SourceCatalogProvider(metaclass=abc.ABCMeta):
 
     @abc.abstractmethod
     def get_source_catalog(self, imagename):
@@ -22,7 +25,8 @@ class SourceCatalogProvider (metaclass=abc.ABCMeta):
         '''
         pass
 
-class e91SourceCatalogProvider (SourceCatalogProvider):
+
+class e91SourceCatalogProvider(SourceCatalogProvider):
 
     def get_source_catalog(self, imagename):
         e91image = fits.open(imagename)
@@ -47,49 +51,62 @@ class e91SourceCatalogProvider (SourceCatalogProvider):
         return (sourceCatalog, image_wcs)
 
 
-class blindGaiaAstrometrySourceCatalogProvider (SourceCatalogProvider):
+class blindGaiaAstrometrySourceCatalogProvider(SourceCatalogProvider):
     url = 'http://astrometry.lco.gtn/image/'
 
     def get_source_catalog(self, imagename):
         fitsimage = fits.open(imagename)
         ra = None
+        image_wcs = None
         for hdu in fitsimage:
 
             try:
                 ra = hdu.header['CRVAL1']
                 dec = hdu.header['CRVAL2']
+                image_wcs = WCS(hdu.header)
+
                 continue
             except:
-                log.debug ("NO RA/DEC found yet, trying next extension")
+                log.debug("NO RA/DEC found yet, trying next extension")
 
-        log.debug ("RA/Dec of image is: %s %s" % (ra,dec))
-        payload = {'ra': ra, 'dec':dec, 'image_path': imagename}
-        response = requests.post (self.url, json=payload)
-        response = response.json()
-        if 'CD1_1' in response:
-            # Build a WCS, reverse-transform the source list.
-            image_wcs = WCS(naxis=2)
-            image_wcs.wcs.crpix = [response['CRPIX1'],response['CRPIX2']]
-            image_wcs.wcs.crval = [response['CRVAL1'],response['CRVAL2']]
-            image_wcs.wcs.cd = np.zeros ((2,2))
-            image_wcs.wcs.cd[0][0] = response['CD1_1']
-            image_wcs.wcs.cd[0][1] = response['CD1_2']
-            image_wcs.wcs.cd[1][0] = response['CD2_1']
-            image_wcs.wcs.cd[1][1] = response['CD2_2']
-            image_wcs.wcs.ctype =  [response['CTYPE1'],response['CTYPE2']]
+        log.debug("RA/Dec of image is: %s %s" % (ra, dec))
 
-            sourceRA = response['matched_sources']['ra']
-            sourceDec = response['matched_sources']['dec']
-            sourcex, sourcey =  image_wcs.all_world2pix(sourceRA, sourceDec, 1)
+        # No WCS? Get us one! Or always refine it :-)
+        if not 0:
+            payload = {'ra': ra, 'dec': dec, 'image_path': imagename}
+            response = requests.post(self.url, json=payload)
+            response = response.json()
+            if 'CD1_1' in response:
+                # Build a WCS, reverse-transform the source list.
+                image_wcs = WCS(naxis=2)
+                image_wcs.wcs.crpix = [response['CRPIX1'], response['CRPIX2']]
+                image_wcs.wcs.crval = [response['CRVAL1'], response['CRVAL2']]
+                image_wcs.wcs.cd = np.zeros((2, 2))
+                image_wcs.wcs.cd[0][0] = response['CD1_1']
+                image_wcs.wcs.cd[0][1] = response['CD1_2']
+                image_wcs.wcs.cd[1][0] = response['CD2_1']
+                image_wcs.wcs.cd[1][1] = response['CD2_2']
+                image_wcs.wcs.ctype = [response['CTYPE1'], response['CTYPE2']]
 
-            return (Table([sourcex,sourcey], names=['x','y'])), image_wcs
+        # We now have a good first order wcs, let's find all thje sources in the image.
 
-        else:
-            return None, None
+        # sourceRA = response['matched_sources']['ra']
+        # sourceDec = response['matched_sources']['dec']
+        # sourcex, sourcey =  image_wcs.all_world2pix(sourceRA, sourceDec, 1)
 
+        image_data = fitsimage[1].data
+        image_data = image_data.astype(float)
+
+        backGround = sep.Background(image_data)
+        log.info("Background: %f \pm %f " % (backGround.globalback, backGround.globalrms))
+        objects = sep.extract(image_data - backGround, 10, backGround.globalrms)
+
+        retTable = Table([objects['x'], objects['y']], names=['x', 'y'])
+        return retTable, image_wcs
 
 
 if __name__ == '__main__':
     sourcecatalogProvider = blindGaiaAstrometrySourceCatalogProvider()
 
-    sourcecatalogProvider.get_source_catalog('/archive/engineering/tlv/ak10/20190124/raw/tlv1m0XX-ak10-20190124-0012-e00.fits.fz')
+    sourcecatalogProvider.get_source_catalog(
+        '/archive/engineering/tlv/ak10/20190124/raw/tlv1m0XX-ak10-20190124-0012-e00.fits.fz')
